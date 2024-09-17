43c29ffdad71664e58f7fb55cd9cf8a09819b89f
Pavan Uthsara <uthsarapavan@gmail.com>
2024-09-17 12:02:55 +0530

    arrayList example 2 : some methods that can use with arraylist

--- src/pkg1/Demo.java
+++ src/pkg1/Demo.java
@@ -26,6 +26,44 @@ public class Demo {
 		for(int n : nums) {
 			System.out.println(n);
 		}
+		
+		//ArrayList example 2
+		ArrayList<String> fruits = new ArrayList<>();
+		fruits.add("Mango");
+		fruits.add("Orange");
+		fruits.add("PineApple");
+		
+		System.out.println(fruits);
+		
+		//Add item to array list
+		fruits.add(0, "This is 0 th index"); // This will be the first element 
+		System.out.println(fruits);
+		
+		//Get method
+		System.out.println(fruits.get(2)); //This should output the 2nd index element
+		System.out.println(fruits.getFirst());
+		System.out.println(fruits.getLast());
+		System.out.println(fruits.indexOf("Mango")); //This outputs the index of element "Mango"
+
+		//Remove item by index
+		fruits.remove(0);
+		System.out.println(fruits);
+		
+		//Remove item by value'
+		fruits.remove("Orange");
+		System.out.println(fruits);
+		
+		//Get the size of array list
+		System.out.println(fruits.size());
+		
+		//Change the item value in array list 
+		fruits.set(0, "Apple"); // 0th index elements replaces with "Apple"
+		System.out.println(fruits);
+		
+		//Remove all the elements from array list
+		fruits.clear();
+		System.out.println(fruits);
+		System.out.println(fruits.size());
 	}
 
 }
